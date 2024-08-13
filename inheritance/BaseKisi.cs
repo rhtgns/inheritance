d3b5e89f10bbaca09c0325ee56d3078132426cff
rohat güneş <rohatgns44@gmail.com>
2024-08-13 13:29:26 +1000

    Add project files.

---
+++ inheritance/BaseKisi.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    public class BaseKisi
+    {
+
+
+        public string Name { get; set; }
+
+
+        public string SurName { get; set; }
+
+        public void ReadNameSurname()
+        {
+            Console.WriteLine($" Name :{Name} , Surname : {SurName} ");
+        }
+    }     
+        
+        public class Student : BaseKisi 
+        {
+
+            public int StudentNo { get; set; }
+            
+            //burda kullanıcıdan gelen isim soy isimleri yazdırıyortuz
+
+             public void ReadNameSurnameStudent() 
+           { 
+             ReadNameSurname();
+
+
+
+            Console.WriteLine($"ogrenci no: {StudentNo}");
+        }
+           
+
+           
+       
+
+        }
+        public class Teacher : BaseKisi
+        {
+         
+         public double TeacherSalary {  get; set; }
+
+
+
+
+        // Maaş bilgisi, Ad ve Soyad bilgilerini konsola yazdıralım
+
+         public void ReadTeacherInfo()
+        {
+            ReadNameSurname();
+
+            Console.WriteLine($" maas :{TeacherSalary}");
+        }
+
+    }
+
+}
+
---
+++ inheritance/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using inheritance;
+
+class BaseKisi
+{
+    static void Main(string[] args)
+    {
+
+        Student student = new Student
+        {
+            Name = "Rohat",
+            SurName = "Gunes",
+            StudentNo = 509
+        };
+        Teacher teacher = new Teacher
+        {
+            Name = "leyla",
+            SurName = "Şen",
+            TeacherSalary = 32000
+        };
+
+        Console.WriteLine("ogrencı  bilgileri");
+        student.ReadNameSurnameStudent();
+        teacher.ReadTeacherInfo();
+
+
+    }
+
+    }
